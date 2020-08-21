c2e6283094d5c79ddf19deba049b52c6b0a5906a
Mac Gibbons <macgibbons@gmail.com>
2020-08-21 11:47:34 -0500

    mikro ORM setup

---
+++ dist/constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.__userPW__ = exports.__prod__ = void 0;
+exports.__prod__ = process.env.NODE_ENV === 'production';
+exports.__userPW__ = 'postgres';
+//# sourceMappingURL=constants.js.map
