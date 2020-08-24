23c6c48817c28ec2ee3028684ff2b1cf6d5055cf
Mac Gibbons <macgibbons@gmail.com>
2020-08-24 16:59:11 -0500

    client and server in single project

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.__userPW__ = exports.__prod__ = void 0;
-exports.__prod__ = process.env.NODE_ENV === 'production';
-exports.__userPW__ = 'postgres';
-//# sourceMappingURL=constants.js.map
